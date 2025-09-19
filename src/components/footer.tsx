8d211b52eb6e382ab8ef4fbaa8dbfce6c8cb2a7d
Thomas Granbohm <thomas.granbohm@gmail.com>
2025-09-20 00:33:11 +0200

    Update Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,17 @@
 export const Footer = () => (
 	<footer className="mx-auto h-min max-w-xl px-2 py-4">
-		<p className="text-center text-sm text-black dark:text-neutral-50">
+		<p className="text-foreground overflow-hidden text-center text-sm">
 			Är det något som strular? <br />
-			Mejla mig på{" "}
+			Mejla thomas [snabel-a] granbohm [punkt] rocks eller{" "}
 			<span className="inline-block">
-				thomas [snabel-a] granbohm [punkt] rocks
-			</span>{" "}
-			eller{" "}
-			<a
-				className="inline-block text-blue-600 hover:underline dark:text-blue-400"
-				href="https://github.com/thomasgranbohm/tideemit"
-			>
-				hjälp till
-			</a>
-			!
+				<a
+					className="text-blue-600 hover:underline dark:text-blue-400"
+					href="https://github.com/thomasgranbohm/tideemit"
+				>
+					hjälp till
+				</a>
+				!
+			</span>
 		</p>
 	</footer>
 );
