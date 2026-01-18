52a72a46b37ea1ea9c53a62de056215f98687653
Thomas Granbohm <thomas.granbohm@gmail.com>
2026-01-18 02:22:57 +0100

    Update contact info

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => (
 	<footer className="mx-auto h-min max-w-xl px-2 py-4">
 		<p className="overflow-hidden text-center text-sm text-neutral-900 dark:text-neutral-50">
 			Är det något som strular? <br />
-			Mejla thomas [snabel-a] granbohm [punkt] rocks eller{" "}
+			Mejla kontakt [snabel-a] tideemit [punkt] net eller{" "}
 			<span className="inline-block">
 				<a
 					className="text-blue-600 hover:underline dark:text-blue-400"
